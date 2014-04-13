1f1a3adeca39caed61e96e1c8bc0e4185205059a
blukat <yunjong@kaist.ac.kr>
2014-04-13 23:55:20 +0900

    Disable auto-install tmux. Add "source .bashrc"

--- config-blukat.sh
+++ config-blukat.sh
@@ -10,6 +10,7 @@ if [ -f $BASE/.bashrc ]; then
     mv $BASE/.bashrc $BASE/.bashrc.old
 fi
 cp ./blukat.bashrc $BASE/.bashrc
+source $BASE/.bashrc
 
 if [ -f $BASE/.tmux.conf ]; then
     mv $BASE/.tmux.conf $BASE/.tmux.conf.old
@@ -17,8 +18,8 @@ fi
 cp ./blukat.tmux.conf $BASE/.tmux.conf
 
 type -a tmux
-if [[ $? != 0 ]]; then
-  sh tmux_local_install.sh
+if [ $? != 0 ]; then
+  echo "tmux is not installed. run \"sh tmux_local_install.sh\""
 fi
 
 echo "Config complete. Undo config using unconfig-blukat.sh"

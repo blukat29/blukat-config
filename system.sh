963d233ed047c5ff6196fa2f10f49df6241fa5dd
blukat29 <yunjong@kaist.ac.kr>
2014-08-22 00:09:16 +0900

    create system.sh with apt_repo

---
+++ system.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+head() {
+  echo ""
+  echo "\033[1;33m$1\033[0;0m"
+}
+
+info() {
+  echo "\033[1;36m$1\033[0;0m"
+}
+
+prompt() {
+  while true; do
+    read -p "  continue? [y/n] " yn
+    case $yn in
+      [yY] ) return "1";;
+      [nN] ) return "0";;
+      * ) echo "Please answer y or n.";;
+    esac
+  done
+}
+
+apt_repo() {
+  head "Change apt-get repository into kr.archive.ubuntu.com"
+  info "  files affected: /etc/apt/sources.list"
+  prompt
+  if [ $? -eq 1 ]; then
+    cd /etc/apt
+    sudo cp sources.list sources.list.bak
+    awk -v OLD=us.archive.ubuntu.com \
+        -v NEW=kr.archive.ubuntu.com \
+        'BEGIN {count=0;}
+         ($0 ~ OLD) {gsub(OLD, NEW); count++;}
+         END {print count " lines changed." > "/dev/stderr"}' \
+        sources.list > sources.list.tmp
+    mv sources.list.tmp sources.list
+    echo "done."
+  fi
+}
+
+apt_repo
+

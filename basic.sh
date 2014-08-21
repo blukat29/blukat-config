54ddc6624437497255e458cbd79718c00c853817
blukat29 <yunjong@kaist.ac.kr>
2014-08-21 18:26:58 +0900

    Create basic.sh. Uses symlink to deploy dotfiles.

---
+++ basic.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname $(readlink -e $0))
+BACKUP_DIR=$HOME/.config.bak.d
+
+# Original dotfiles in home directory are copied into $BACKUP_DIR
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir "$BACKUP_DIR"
+fi
+relpath() {
+  python -c "import os.path; print os.path.relpath('$1','${2:-$PWD}')"
+}
+setup () {
+  OLD="$HOME/.$1"
+  BAK="$BACKUP_DIR/.$1"
+  NEW="$SCRIPT_DIR/dotfiles/$1"
+  if [ -f $OLD ]; then
+    if [ -L $OLD ]; then
+      rm $OLD
+    else
+      mv $OLD $BAK
+    fi
+  fi
+  ln -s $(relpath $NEW $HOME) $OLD
+}
+
+setup bashrc
+setup vimrc
+setup tmux.conf
+setup gitconfig
+

076d8a2ccddef3874e666f76f2e2c11a5534952a
blukat29 <yunjong@kaist.ac.kr>
2014-11-12 12:43:33 +0900

    Update memstat and put in tmux.conf

--- scripts/memstat.py
+++ scripts/memstat.py
@@ -1,21 +1,9 @@
 import sys
 
-total = sys.stdin.readline().split()[:2]
-used = sys.stdin.readline().split()[:2]
-
-unit = {"K":1, "M":1024, "G":1024*1024}
-
-total = int(total[0]) * unit[total[1]]
-used = int(used[0]) * unit[used[1]]
-
-def sizefmt(n):
-    if n < 1024:
-        return "%d kB" % n
-    n = n / 1024.0
-    if n < 2048:
-        return "%d MB" % n
-    n = n / 1024.0
-    return "%.2f GB" % n
-
-print "%s / %s" % (sizefmt(used), sizefmt(total))
+total = int(sys.stdin.readline().split()[1])
+free = int(sys.stdin.readline().split()[1])
+used = total - free
+rate = int(used*100.0/total)
+s = "%.1f/%.1fG(%d%%)" % (used/1048576.0, total/1048576.0, rate)
+print "%14s" % s
 

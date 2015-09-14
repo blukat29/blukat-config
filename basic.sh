f10cbf11ac5d9c52f53ccf9bb0db6ee39d250a08
blukat29 <yunjong@kaist.ac.kr>
2015-09-15 00:46:37 +0900

    Generate /usr/include tags at basic setup

--- basic.sh
+++ basic.sh
@@ -34,3 +34,5 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+ctags -R --sort=yes --fields=iaSl --c++-kinds=+p --extra=+q -f ~/.vim/local /usr/include/
+

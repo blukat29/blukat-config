67737126d583773bc413ea2fe0af3e94be33313b
blukat29 <yunjong@kaist.ac.kr>
2016-04-19 16:14:54 +0900

    Fix uptime for more than a day

--- scripts/uptime.py
+++ scripts/uptime.py
@@ -2,6 +2,7 @@ import sys
 import re
 f = sys.stdin.read()
 uptime, loadavg = re.search("up (.*)\d+ users.*load average: (.*)", f).groups()
-uptime = uptime.lstrip().rstrip().rstrip(',')
+uptime = uptime.strip().rstrip(',')
+uptime = re.sub(",\s+", " ", uptime)
 loadavg = loadavg.replace(',','')
 sys.stdout.write("[up %s][%s]" % (uptime, loadavg))

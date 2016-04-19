6a1f0c8f79c9f1c83c2f93473f1e77a78b7608da
blukat29 <yunjong@kaist.ac.kr>
2016-04-19 15:41:48 +0900

    Improve tmux status bar for Linux

--- scripts/tmux_status.sh
+++ scripts/tmux_status.sh
@@ -5,6 +5,8 @@ SYSTEM=`uname -s`
 case "$SYSTEM" in
     "Darwin") top -l 1 -s 0 | grep PhysMem | python $SCRIPTS_DIR/memstat.py Darwin
         ;;
-    "Linux") cat /proc/meminfo | grep Mem | python $SCRIPTS_DIR/memstat.py Linux
+    "Linux")
+        echo -n "[$(cat /proc/loadavg | cut -c -14)]"
+        echo -n "[$(cat /proc/meminfo | grep Mem | python $SCRIPTS_DIR/memstat.py Linux)]"
         ;;
 esac

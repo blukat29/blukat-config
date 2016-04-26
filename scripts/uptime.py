b8fd0c61ba15e4821f3abbd1e3656ddd859413d6
blukat29 <yunjong@kaist.ac.kr>
2016-04-27 00:14:38 +0900

    Fix uptime.py

--- scripts/uptime.py
+++ scripts/uptime.py
@@ -1,7 +1,7 @@
 import sys
 import re
 f = sys.stdin.read()
-uptime, loadavg = re.search("up (.*)\d+ users.*load average: (.*)", f).groups()
+uptime, loadavg = re.search("up (.*)\d+ user.*load average: (.*)", f).groups()
 uptime = uptime.strip().rstrip(',')
 uptime = re.sub(",\s+", " ", uptime)
 loadavg = loadavg.replace(',','')

a21abe426104a4d9e4b94935c6d36dfc2b1d1578
blukat29 <yunjong@kaist.ac.kr>
2016-06-27 17:34:58 +0900

    Update cgc2elf

--- scripts/cgc2elf.py
+++ scripts/cgc2elf.py
@@ -1,12 +1,14 @@
 import sys
 
+magic = "\177ELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
 def convert_file(name):
     with open(name, 'rb') as f:
         data = f.read()
     elfname = name + ".elf"
     with open(elfname, 'wb') as g:
-        g.write("\x7fELF")
-        g.write(data[4:])
+        g.write(magic)
+        g.write(data[len(magic):])
 
 if __name__ == '__main__':
     for name in sys.argv[1:]:

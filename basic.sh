619dfb5bee8965f782564ed1b48cf9e52c2463ca
blukat29 <yunjong@kaist.ac.kr>
2016-11-15 20:07:23 +0900

    install vundle plugins by default

--- basic.sh
+++ basic.sh
@@ -36,6 +36,7 @@ setup gdbinit
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+vim +VundleInstall +qall
 if [ ! -d ~/.local/peda ]; then
   git clone https://github.com/longld/peda ~/.local/peda
 fi

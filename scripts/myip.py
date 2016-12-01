5404969ea05eb410b4834a7198ca75884e7b651e
blukat29 <yunjong@kaist.ac.kr>
2016-12-01 12:56:17 +0900

    Add httpbin_ip to myip.py

--- scripts/myip.py
+++ scripts/myip.py
@@ -25,22 +25,48 @@ def host_ip():
             return ip
     return None
 
+def httpbin_ip():
+    import json
+    try:
+        from urllib.request import urlopen
+    except ImportError:
+        from urllib2 import urlopen
+    r = urlopen('http://httpbin.org/ip')
+    return json.loads(r.read().decode('utf-8'))['origin']
+
 def benchmark():
+
     import time
     milis = lambda: int(round(time.time() * 1000))
-    n1 = 1000
-    n2 = 50
+    def do_test(f, n):
+        t0 = milis()
+        for i in range(n): f()
+        dt = float(milis() - t0) / n
+        print('%s (%s) took %.4fms per request.' % (f.__code__.co_name, f(), dt))
 
-    t0 = milis()
-    for i in range(n1): sock_ip()
-    print('sock_ip took %.4f ms per request.' % (float(milis() - t0)/ n1))
-
-    t0 = milis()
-    for i in range(n2): host_ip()
-    print('host_ip took %.4f ms per request.' % (float(milis() - t0)/ n2))
+    do_test(sock_ip, 1000)
+    do_test(host_ip, 50)
+    do_test(httpbin_ip, 3)
 
 if __name__ == '__main__':
+    usage='''\
+myip.py [-shtb]
+  -s: connect to 8.8.8.8:53 and use getsockname(). Generally fastest.
+  -h: use gethostbyname(gethostname()). Usually don't work.
+  -t: get http://httpbin.ip. Fetches public IP, but slow.
+  -b: Benchmark above methods.'''
     if len(sys.argv) > 1:
-        benchmark()
+        opt = sys.argv[1]
+        if len(opt) != 2 or opt[0] != '-' or (opt[1] not in 'shtb'):
+            print(usage)
+            exit(1)
+        if opt[1] == 's':
+            print(sock_ip())
+        elif opt[1] == 'h':
+            print(host_ip())
+        elif opt[1] == 't':
+            print(httpbin_ip())
+        elif opt[1] == 'b':
+            benchmark()
     else:
-        print(sock_ip() or host_ip() or "Cannot resolve my IP")
+        print(sock_ip() or host_ip() or httpbin_ip() or "Cannot resolve my IP")

eeca42c14e36f54a0aac922e25a13b62743f5e70
Yunjong Jeong <yunjong@kaist.ac.kr>
2017-06-01 23:09:19 +0900

    Reduce number of apt-get

--- tools/ubuntu-dev.bash
+++ tools/ubuntu-dev.bash
@@ -6,26 +6,30 @@ CURRENT_DIR="$( cd "$( dirname $(readlink -e "${BASH_SOURCE[0]}") )" && pwd )"
 source $CURRENT_DIR/common.bash
 
 doit sudo apt update
+
+# Build tools
 # build-essential covers gcc, g++, libc6-dev, make, dpkg-dev.
-doit sudo apt install -y build-essential gcc-multilib gdb
-# Advanced build tools
-doit sudo apt install -y cmake autoconf automake libtool
+doit sudo apt install -y build-essential gcc-multilib gdb \
+    cmake autoconf automake libtool ninja-build
 # Utilities
-doit sudo apt install -y curl wget git zip p7zip
-doit sudo apt install -y vim vim-nox-py2 tmux exuberant-ctags silversearcher-ag bc
-doit sudo apt install -y tree htop
-doit sudo apt install -y sqlite3 libsqlite3-dev
+doit sudo apt install -y curl wget git zip p7zip \
+    vim tmux exuberant-ctags silversearcher-ag bc \
+    tree htop \
+    sqlite3 libsqlite3-dev
+
 # Python development
 doit sudo apt install -y python-minimal python2.7 python3 python-dev python3-dev
-if type "pip" &> /dev/null; then
+if type "pip2" &> /dev/null || type "pip3" &> /dev/null; then
     msg "pip is already installed"
 else
     doit curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
     doit sudo -H python2 /tmp/get-pip.py
     doit sudo -H python3 /tmp/get-pip.py
 fi
+
 # Python packages
 py23 requests virtualenv virtualenvwrapper jedi isort flake8
+
 # fzf auto completion
 if type "fzf" &> /dev/null; then
     msg "fzf is already installed"

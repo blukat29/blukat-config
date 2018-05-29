d56fbed7866186ddeba17b6551aa3f5e98ebe8d4
blukat29 <yunjong@kaist.ac.kr>
2018-05-29 09:46:57 +0900

    Use kakao ubuntu mirror

--- tools/change-apt-source.sh
+++ tools/change-apt-source.sh
@@ -3,5 +3,5 @@ cp $TARGET $TARGET.bak
 cat $TARGET | awk '
 $0 !~ /^deb/ { print $0; next; }
 $0 ~ /security/ { print $0; next; }
-$0 ~ /^deb/ { print $1, "http://kr.archive.ubuntu.com/ubuntu/", $3, $4, $5, $6; }' > $TARGET.new
+$0 ~ /^deb/ { print $1, "http://mirror.kakao.com/ubuntu/", $3, $4, $5, $6; }' > $TARGET.new
 sed 's/\s\+$//' $TARGET.new > $TARGET

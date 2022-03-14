eb78904f50f11f9bb0b5f5c2d51b3daa62e2e1d1
Yunjong Jeong <yunjong@kaist.ac.kr>
2022-03-14 11:52:03 +0900

    basic.sh install gitconfig at XDG_CONFIG_HOME

--- basic.sh
+++ basic.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=$(dirname $0)
-BACKUP_DIR=$HOME/.config.bak.d
 
 # Original dotfiles in home directory are copied into $BACKUP_DIR
+if [ -z "$XDG_CONFIG_HOME" ]; then
+  XDG_CONFIG_HOME="$HOME/.config"
+fi
+BACKUP_DIR=$XDG_CONFIG_HOME/config.bak.d
 if [ ! -d "$BACKUP_DIR" ]; then
   mkdir "$BACKUP_DIR"
 fi
@@ -18,24 +21,37 @@ relpath() {
   fi
 }
 setup () {
-  OLD="$HOME/.$1"
-  BAK="$BACKUP_DIR/.$1"
-  NEW="$SCRIPT_DIR/dotfiles/$1"
-  if [ -f $OLD ]; then
-    if [ -L $OLD ]; then
-      rm $OLD
-    else
-      mv $OLD $BAK
-    fi
+  NAME=$(basename $1)
+  SOURCE="$SCRIPT_DIR/dotfiles/$1"
+
+  if [ -z "$2" ]; then
+    TARGET="$HOME/.$1"
+  else
+    TARGET="$2"
   fi
-  ln -s $(relpath $NEW $HOME) $OLD
+  TARGET_DIR=$(dirname "$TARGET")
+
+  # Evict old file
+  if [ -L $TARGET ]; then
+    echo "Remove $TARGET"
+    rm $TARGET
+  elif [ -f $TARGET ]; then
+    echo "Evict  $TARGET -> $BACKUP_DIR/$NAME"
+    mv $TARGET $BACKUP_DIR/$NAME
+  fi
+
+
+  # Create symlink into dotfiles dir
+  mkdir -p $(dirname $TARGET)
+  ln -s $(relpath $SOURCE $TARGET_DIR) $TARGET
+  echo "Link   $TARGET -> $SOURCE"
 }
 
 setup bashrc
 setup vimrc
 setup tmux.conf
-setup gitconfig
-setup gitconfig.common
+setup gitconfig        $XDG_CONFIG_HOME/git/config
+setup gitconfig.common $XDG_CONFIG_HOME/git/gitconfig.common
 setup gitignore
 setup gdbinit
 setup inputrc
@@ -49,12 +65,14 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
 fi
 vim +PlugInstall +qall
 
-# fzf auto completion
+# Install fzf auto completion
 type "fzf" &> /dev/null
-if [ -n "$?" ]; then
+if [ $? -ne 0 ]; then
     git clone https://github.com/junegunn/fzf "$HOME/.config/fzf"
     "$HOME/.config/fzf/install" --no-update-rc --completion --key-bindings
     mv "$HOME/.fzf.bash" "$HOME/.config/.fzf.bash"
+else
+    echo "fzf already installed"
 fi
 
 # Prompt git user if not set
